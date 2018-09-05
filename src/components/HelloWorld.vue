ca9a2bd64e67eea7ab60a3d373d61708d9b651c9
lzx <466764826@qq.com>
2018-09-05 11:15:46 +0800

    第二次提交

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p><router-link to="/First">去第一个页面</router-link></p>
+    <el-header><h1>{{ msg }}</h1></el-header>
+    <el-main><p><router-link to="/First">去第一个页面</router-link></p></el-main>
+    <el-footer>底部</el-footer>
   </div>
 </template>
 
@@ -18,8 +19,12 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+body{
+  margin: 0;
+}
 h1, h2 {
   font-weight: normal;
+  margin: 0;
 }
 a {
   color: #42b983;
